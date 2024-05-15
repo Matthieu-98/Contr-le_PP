dbcd29e3edc2c59ca3207a4f45bd7e7ad1aaeb0c
Matthieu <mohimbouabeka@gmail.com>
2024-05-15 13:02:43 +0200

    Exo 5

---
+++ LostSheep.js
@@ -0,0 +1,12 @@
+const lostSheep = (tab1, tab2, nb) => {
+    somme = (element)
+    tab1.map();
+
+    if (tab1.concat(tab2) < nb) {
+        return (nb - tab1.concat(tab2))
+    }
+
+
+} 
+
+console.log([5,1,3], [3,6,1], 30)
